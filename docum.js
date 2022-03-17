b9b79bb102d8807b29f17043040befb73626dd4e
unknown <188r1a1202@gmail.com>
2022-03-17 21:27:14 +0530

    java scripts

---
+++ docum.js
@@ -0,0 +1,2 @@
+ document.write("hello this is separeteed along one");
+ 
---
+++ too many arguments.asm
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>www.shiva.com</title>
+</head>
+<body>
+<script>
+document.write("first line <br>");
+function add(a,b)
+{
+document.write("A:"+a+"B:"+b+"c:"+arguments[]};
+
+
+
+}
+ add(10,20,30);
+</script>
+
+
+
+</body>
+</html>
